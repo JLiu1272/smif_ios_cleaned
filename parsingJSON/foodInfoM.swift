4dded8219afd6c016c4496634739ea1b977d3f7e
JenniferLiu <jwt7689@rit.edu>
2017-04-02 00:41:21 -0400

    Removed FoodInfoM for now

--- parsingJSON/foodInfoM.swift
+++ parsingJSON/foodInfoM.swift
@@ -10,36 +10,4 @@ import UIKit
 
 class foodInfoM: NSObject {
     
-    //properties
-    var id: Int?
-    var name: String?
-    var aisle: String?
-    var image: String?
-    var amount: String?
-    var unit: String?
-    var nutrition:[]
-    //var image: UIImage?
-    
-    //Empty Constructor
-    override init(){
-        
-    }
-    
-    init(id: Int, title: String, image: String?, usedIngredientCount: Int, missedIngredientCount: Int)
-    {
-        
-        self.id = id
-        self.title = title
-        self.image = image
-        self.usedIngredientCount = usedIngredientCount
-        self.missedIngredientCount = missedIngredientCount
-        //self.image = image
-        
-    }
-    
-    //print object's current state
-    override var description: String{
-        return "Id: \(String(describing: id)), Title: \(String(describing: title)), Image: \(String(describing: image)), Used Ingredient Count: \(String(describing: usedIngredientCount)), Missed Ingredient Count: \(String(describing: missedIngredientCount))"
-    }
-
 }
